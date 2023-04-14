79324935fd5714cd3a1dd08dde082e63fce35af3
Gethin-a <126208350+Gethin-a@users.noreply.github.com>
2023-04-14 17:45:18 +0100

    Add files via upload

---
+++ VehicleSpawner-master/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Gothing#7524'
+description 'A custom addon vehicle spawn menu that uses a game chat command.'
+
+client_scripts {
+    'Client.net.dll',
+}
+
+files {
+    'SharpConfig.dll',
+    'NativeUI.dll',
+    'config.ini',
+}
